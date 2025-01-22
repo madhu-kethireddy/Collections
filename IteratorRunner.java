d0c26587dfede00a66399536da6ef53cc8d28e4d
madhu kethireddy <madhukethireddy.k@gmail.com>
2025-01-22 11:21:11 +0530

    refactor the Iterator file

--- IteratorRunner.java
+++ IteratorRunner.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class IteratorRunner {
     public static void main(String[] ats){
-        List<String> wordsList = List.of("yellow", "blue", "green", "red");
+        List<String> wordsList = List.of("yellow", "blue", "green", "red", "cat");
         System.out.println(wordsList);
+
+        List<String> arrList = new ArrayList<>(wordsList);
 
         Iterator<String> words = wordsList.iterator();
         while(words.hasNext()){
             System.out.println(words.next());
         }
+
+        Iterator<String> iterator = arrList.iterator();
+        while(iterator.hasNext()){
+            if(iterator.next().contains("e")){
+                iterator.remove();
+            }
+        }
+        System.out.println(arrList);
     }
 }
