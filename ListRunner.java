8b7cd8635a34c55e482536e0a96797d8f6a17a6c
madhu kethireddy <madhukethireddy.k@gmail.com>
2025-01-22 09:45:27 +0530

    Initial commit

---
+++ ListRunner.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+class ListRunner{
+    public static void main(String []a){
+        List<String> words = List.of("Apple", "Bat", "Cat");
+        System.out.println(words);
+    }
+}
