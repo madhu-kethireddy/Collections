037c1b8f79c433d1e83d889d134690007c899996
madhu kethireddy <madhukethireddy.k@gmail.com>
2025-01-22 09:52:15 +0530

    added some list builtin methods

--- ListRunner.java
+++ ListRunner.java
@@ -4,5 +4,12 @@ class ListRunner{
     public static void main(String []a){
         List<String> words = List.of("Apple", "Bat", "Cat");
         System.out.println(words);
-    }
+        System.out.println(words.size());
+        System.out.println(words.get(1));
+        System.out.println(words.contains("Madhu"));
+        System.out.println(words.isEmpty());
+        System.out.println(words.indexOf("Apple"));
+        System.out.println(words.contains("Dog"));
+        
+    } 
 }
