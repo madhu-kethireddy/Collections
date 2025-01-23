32fb802bacd127ce408f21638020a3e6af81e7f8
madhu kethireddy <madhukethireddy.k@gmail.com>
2025-01-23 16:56:30 +0530

    feat: added HashSet & Anonymous set file

---
+++ HashSetRunner.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetRunner {
+    static public void main(String[] ars){
+
+        Set<Number> hasSet = new HashSet<Number>(){
+            {
+            this.add(3.23f);
+            this.add(0);
+            this.add(2);   
+            }   
+        };
+        hasSet.add(32);
+        hasSet.add(302);
+        hasSet.add(92);
+        hasSet.add(342);
+        hasSet.add(4.50f);
+        hasSet.add(12);
+        System.err.println(hasSet);
+
+    }
+}
